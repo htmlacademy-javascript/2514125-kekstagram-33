b95e5ab8270508a7de57c55980828e6412fe825d
Keks <keks@htmlacademy.ru>
2024-11-07 19:17:55 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,4 @@
+import {getPictures} from './data.js';
+import {makePicturesOnPage} from './thumbnails.js';
+
+makePicturesOnPage (getPictures());
---
+++ 7/js/util.js
@@ -0,0 +1,41 @@
+// Получение случайного, положительного числа
+
+const getRandomInteger = function (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+// Проверка на повторяющиеся числа
+
+const createRandomIdFromRangeGenerator = function (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+// Получение случайного элемента из массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// Генерация случайного, уникального ID
+const createIdGenerator = () => {
+  let currentId = 0;
+
+  return () => ++currentId;
+};
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArrayElement, createIdGenerator};
