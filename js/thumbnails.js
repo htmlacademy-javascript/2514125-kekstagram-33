b531f116ae244dca3adae78e549e295dee9b0434
Faretos <165820990+Faretos@users.noreply.github.com>
2024-11-14 18:51:37 +0300

    Module8-task1 (#6)
    * Добавление наставника в readme
    
    * Merge pull request #2 from Faretos/module1-task1
    
    * Нужно больше функций
    
    * Merge remote-tracking branch 'upstream/master' into module2-task1
    
    * небольшое исправление
    
    * Merge pull request #3 from Faretos/module2-task1
    
    * ДЗ Больше деталей модуль 4
    
    * Merge pull request #4 from Faretos/module4-task1
    
    * Модуляция
    
    * Небольшое исправление
    
    *  Функции возвращаются
    
    * Merge branch 'master' into module5-task2
    
    * Merge pull request #5 from Faretos/module5-task1
    
    * Merge pull request #6 from Faretos/module5-task2
    
    * Исправления
    
    *  Создание модуля для отрисовки миниатюр
    
    * Merge branch 'master' into module7-task1
    
    * Добавление отрисовки изображений
    
    * Merge branch 'master' into module7-task1
    
    * Merge remote-tracking branch 'upstream/master' into module7-task1
    
    * Merge pull request #7 from Faretos/module7-task1
    
    *  Добавление отрисовки Больших Изображений
    
    * Merge remote-tracking branch 'upstream/master' into module8-task1
    
    * Исправление замечаний
    
    * Merge branch 'master' into module8-task1

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,13 +1,19 @@
+import {openUserModal} from './bigpicture.js';
+
 const pictureSection = document.querySelector('.pictures');
 const pictureItemTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const createPictures = (({url, description, likes, comments}) => {
+const createPictures = ((data) => {
+  const {url, description, likes, comments} = data;
+
   const pictureElement = pictureItemTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+  pictureElement.addEventListener('click', () => openUserModal(data));
 
   return pictureElement;
 });
