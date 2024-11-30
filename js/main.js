1c50bba2852ec6bf25b4cd15a137af2f62484483
Faretos <vanyafilipiev@gmail.com>
2024-11-30 17:54:39 +0300

    Добавления фильтрации изображений и debounce

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ import './scale-effect.js';
 import './fillter-effect.js';
 import { getData } from './api.js';
 import { createDataErrorMessage } from './result-message-data.js';
+import { debounce } from './util.js';
+import { filterPictures, applyFilter } from './display-filter.js';
 
 const bootstrap = async () => {
   try {
     const photos = await getData();
-    makePicturesOnPage(photos);
+    const debouncedMakePicturesOnPage = debounce(makePicturesOnPage, 500);
+    applyFilter(photos, debouncedMakePicturesOnPage);
+    makePicturesOnPage(filterPictures());
   } catch (error) {
     createDataErrorMessage(error.message);
   }
