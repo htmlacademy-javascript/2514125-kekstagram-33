558b0112ee3f4d2a0e42eeb81233472ad8c0e488
Faretos <vanyafilipiev@gmail.com>
2024-12-05 17:35:50 +0300

    Исправление счетчика комментариев

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -69,7 +69,7 @@ const renderComments = () => {
   }
 
   commentsList.appendChild(fragment);
-  const commentsDisplay = `<span class="social__comment-shown-count">${commentsShown}</span> из <span class="comments-count">${comments.length}</span> комментариев`;
+  const commentsDisplay = `<span class="social__comment-shown-count">${commentsShown}</span> из <span class="social__comment-total-count">${comments.length}</span> комментариев`;
   commentsCount.innerHTML = commentsDisplay;
 };
 
